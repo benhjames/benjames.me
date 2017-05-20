63f025cdaeb2338f0a01e4c26b1d914f6effc967
Ben James <benhjames@sky.com>
2017-05-20 20:14:15 +0100

    Add global.css file to fix styled jsx issue

--- components/HTMLHead.js
+++ components/HTMLHead.js
@@ -25,41 +25,7 @@ const HtmlHead = () => (
     />
     <link type="image/x-icon" rel="icon" href="/static/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-    <style jsx global>{`
-      @font-face {
-        font-family: 'chunkfive';
-        src: url('/static/fonts/chunkfive-webfont.eot');
-        src: url('/static/fonts/chunkfive-webfont.eot?#iefix') format('embedded-opentype'),
-            url('/static/fonts/chunkfive-webfont.woff2') format('woff2'),
-            url('/static/fonts/chunkfive-webfont.woff') format('woff'),
-            url('/static/fonts/chunkfive-webfont.ttf') format('truetype'),
-            url('/static/fonts/chunkfive-webfont.svg#chunkfiveroman') format('svg');
-        font-weight: normal;
-        font-style: normal;
-      }
-
-      body {
-        background-color: #173e43;
-      }
-
-      .row-padding {
-        padding-top: 50px;
-        padding-bottom: 40px;
-      }
-
-      .row-extra-padding {
-        padding-top: 80px;
-        padding-bottom: 70px;
-      }
-
-      .link-secondary {
-        color: #f0ad4e;
-      }
-
-      h1, h2, h3, h4 {
-        font-family: 'chunkfive'
-      }
-    `}</style>
+    <link rel="stylesheet" type="text/css" href="/static/global.css" />
   </Head>
 );
 
